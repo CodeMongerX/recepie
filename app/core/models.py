af573ec273ff2590a9e0bf66ca4068162482d2e7
Rajeev S <raghu2raj@gmail.com>
2019-05-02 21:47:51 +0530

    added superuser

--- app/core/models.py
+++ app/core/models.py
@@ -14,6 +14,15 @@ class userManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+    
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
 
 class user(AbstractBaseUser, PermissionsMixin):
     """custom model thats supports using email instead of username"""
