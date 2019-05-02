96d9c49714470a72eb3277c2637518930f9ac271
Rajeev S <raghu2raj@gmail.com>
2019-05-02 23:26:26 +0530

    added new testcases

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class adminSiteTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@rajeevinc.com',
+            password='test@123'
+        )
+        self.client.force_login(user=self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='someuser@rajeevinc.com',
+            password='test@123',
+            name='someguy'            
+        )
+
+    def test_user_listed(self):
+        """Test users that are listed on thta page"""
+        url = reverse("admin:core_user_changelist")
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
